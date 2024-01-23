4ffbe0a596a0cb20fb5f1f3a46d49e66a773523f
Daniel Mutuku <danielmakau507@gmail.com>
2024-01-23 09:43:54 +0300

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Home from './Pages/Home';
+import InfoListing from './Pages/InfoListing';
+import Login from './Pages/Login';
+import { useState } from 'react';
+import Admin from './Pages/Admin';
+import ViewAllListing from './Pages/ViewAllListing';
+import ViewSingleListing from './Pages/ViewSingleListing';
+import UpdateListing from './Pages/UpdateListing';
+
+const API_URL = 'https://airbnb-react-k4zr.onrender.com/listings' 
 
 function App() {
+  //create a useState for login
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [allListings, setAllListings]=useState([])
+
+  //function to delete listing from DOM
+  const deleteListing = (id)=>{
+    const updatedListings = allListings.filter((listing)=>(listing.id !== id))
+    //console.log(updatedListings)
+    setAllListings(updatedListings)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}></Layout>}>
+          <Route index element={<Home API_URL={API_URL}></Home>}></Route>
+          <Route path='/airbnb/:id' element={<InfoListing API_URL={API_URL}></InfoListing>}></Route>
+          <Route path='/login' element={<Login setIsLoggedIn={setIsLoggedIn}></Login>}></Route>
+          <Route path='/admin' element={<Admin isLoggedIn={isLoggedIn} API_URL={API_URL}></Admin>}></Route>
+          <Route path='/admin/viewall' element={<ViewAllListing isLoggedIn={isLoggedIn} API_URL={API_URL} allListings={allListings} setAllListings={setAllListings}></ViewAllListing>}></Route>
+          <Route path='/admin/viewall/:id' element={<ViewSingleListing isLoggedIn={isLoggedIn} API_URL={API_URL} onDelete={deleteListing}></ViewSingleListing>}></Route>
+          <Route path='/admin/edit/:id' element={<UpdateListing isLoggedIn={isLoggedIn} API_URL={API_URL} ></UpdateListing>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
