8a587caad9e3e53540a5d70212fa13a167351b1e
Daniel Mutuku <danielmakau507@gmail.com>
2024-01-23 11:44:34 +0300

    login register page

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,46 +1,51 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 
 export default function Login(props) {
-  const navigate = useNavigate()
-  const [formData,setFormData] = useState({
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
     username: '',
     password: ''
-  })
-  //function to handle change of input data
-  const handleChange =(e)=>{
+  });
+
+  //handle change of input data
+  const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value 
-    })
-  }
-  //function to handle form submit
-  const handleSubmit = (e)=>{
-    e.preventDefault()
-    props.setIsLoggedIn(true)
-    navigate('/admin')
-  }
+      [e.target.name]: e.target.value
+    });
+  };
+
+  //handle form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.setIsLoggedIn(true);
+    navigate('/admin');
+  };
 
   return (
     <div className='container'>
-      <div className='row'>
+      <div className='row d-flex align-items-center justify-content-center' style={{ minHeight: '80vh' }}>
         <div className='col-md-6'>
-          <form onSubmit={handleSubmit}>
+          <form className='bg-secondary bg-opacity-25 p-4' style={{borderRadius: '15px'}} onSubmit={handleSubmit}>
+            <div>
+              <h2 className='text-center'>SignIn</h2>
+            </div>
             <div className="mb-3">
               <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" aria-describedby="emailHelp" name='username' onChange={handleChange} value={formData.username} required/>
+              <input type="email" className="form-control" aria-describedby="emailHelp" name='username' onChange={handleChange} value={formData.username} required />
             </div>
             <div className="mb-3">
               <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-              <input type="password" className="form-control" name='password' onChange={handleChange} value={formData.password} required/>
+              <input type="password" className="form-control" name='password' onChange={handleChange} value={formData.password} required />
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <div className='mb-3'>
+              <p>Don't have an account? <Link to='/register'>Register here</Link></p>
+            </div>
+            <button type="submit" className="btn btn-primary">Login</button>
           </form>
-        </div>
-        <div>
-          {/* nothing here */}
         </div>
       </div>
     </div>
-  )
+  );
 }
