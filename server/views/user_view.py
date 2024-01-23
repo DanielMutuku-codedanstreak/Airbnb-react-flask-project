074861d23e210927e97bec61cd9a900c48756f19
Ian Kimani <ianmwng@gmail.com>
2024-01-23 21:01:27 +0300

    changes user_view.py

--- server/views/user_view.py
+++ server/views/user_view.py
@@ -5,4 +5,33 @@ user_bp = Blueprint('user_bp',__name__)
 
 
 #add user (register)
+@user_bp.route('/signup', methods=['POST'])
+def register_user():
+    data = request.form
+    name = data.get('name')
+    email = data.get('email')
+    phone = data.get('phone')
+    password = data.get('password')
+    user_type = data.get('user_type')  
 
+
+    check_email = User.query.filter_by(email=email).first()
+    if check_email:
+        return jsonify({"error": "Email already exists"}), 400
+
+    check_phone = User.query.filter_by(phone=phone).first()
+    if check_phone:
+        return jsonify({"error": "Phone number already exists"}), 400
+
+    new_user = User(
+        name=name,
+        email=email,
+        phone=phone,
+        password=password,
+        user_type=user_type
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully", "user_type": user_type}), 201
