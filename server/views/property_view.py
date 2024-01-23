d0b8570ad8098bd4723cf7fbf4c7c8534a9fdf98
Ian Kimani <ianmwng@gmail.com>
2024-01-23 10:42:55 +0300

    changes on Property_view

--- server/views/property_view.py
+++ server/views/property_view.py
@@ -39,8 +39,40 @@ def get_all_properties():
 
 
 #view single property
+@prop_bp.route('/properties/<int:property_id>')
+def get_single_property(property_id):
+   property = Property.query.filter_by(id=property_id).first()
+   if not property:
+      return jsonify({"error":"property not found"}),404
+   else:
+      return jsonify([
+      {
+         "id":property.id,
+         "title":property.title,
+         "description":property.description,
+         "image":property.image,
+         "other_images":property.other_images,
+         "price":property.price,
+         "category":property.category,
+         "inclusives":property.inclusives,
+         "amenities":property.amenities,
+         "rules":property.rules,
+         "capacity":property.capacity,
+         "bathrooms":property.bathrooms,
+         "beds":property.beds,
+         "location":property.location,
+         "host":{
+            "name":property.user.name,
+            "email":property.user.email,
+            "phone":property.user.phone,
+            },
+          
+         
+      }]
+   ),200
 
 #search property by title
+
 
 #add property
 
