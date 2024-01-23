a4d92a182f3e31180bcd3978c6617414d8c89165
Ian Kimani <ianmwng@gmail.com>
2024-01-23 12:38:30 +0300

    changes on Property_view.py

--- server/views/property_view.py
+++ server/views/property_view.py
@@ -1,6 +1,6 @@
-from flask import Blueprint,jsonify
-from models import db, Property
-
+from flask import Blueprint,jsonify,request
+from models import db, Property,User
+from sqlalchemy import desc
 
 prop_bp = Blueprint('prop_bp', __name__)
 
@@ -107,6 +107,83 @@ def search_property(search):
       ),200
 
 #add property
+@prop_bp.route('/properties',methods=['POST'])
+def add_property():
+    data = request.get_json()
+    
+    title = data['title']
+    description = data['description']
+    image = data['image']
+    other_images = data['other_images']
+    price = data['price']
+    category = data['category']
+    inclusives = data['inclusives']
+    amenities = data['amenities']
+    rules = data['rules']
+    capacity = data['capacity']
+    bathrooms = data['bathrooms']
+    beds = data['beds']
+    location = data['location']
+    
+    user_id=2 #current user id
+    user=User.query.filter_by(id=user_id).first()
+    if user.user_type == 'guest':
+       return jsonify({"error":"unauthorized access"}),404
+    else:
+      new_property = Property(
+          title=title.title(),
+          description=description,
+          image=image,
+          other_images=other_images,
+          price=price,
+          category=category,
+          inclusives=inclusives,
+          amenities=amenities,
+          rules=rules,
+          capacity=capacity,
+          bathrooms=bathrooms,
+          beds=beds,
+          location=location.title(),
+          user_id=user_id
+          
+          
+       )
+      db.session.add(new_property)
+      db.session.commit()
+      
+      property = Property.query.order_by(desc(Property.id)).first()
+      
+      return jsonify([
+      {
+         "id":property.id,
+         "title":property.title,
+         "description":property.description,
+         "image":property.image,
+         "other_images":property.other_images,
+         "price":property.price,
+         "category":property.category,
+         "inclusives":property.inclusives,
+         "amenities":property.amenities,
+         "rules":property.rules,
+         "capacity":property.capacity,
+         "bathrooms":property.bathrooms,
+         "beds":property.beds,
+         "location":property.location,
+         "host":{
+            "name":property.user.name,
+            "email":property.user.email,
+            "phone":property.user.phone,
+            },
+          
+         
+      }]
+   ),201
+
+
+    
+
+    
+
 
 #update property
 
